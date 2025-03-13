5affda03bcdea7dd8f4b5f5a3e120fd5f3ef81df
Uran <yyul960101@gmail.com>
2025-03-13 20:55:48 +0900

    edit vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', 
+    port: 5173
+  },
   assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.png', '**/*.svg'] // ✅ 이미지 파일 허용
 });
