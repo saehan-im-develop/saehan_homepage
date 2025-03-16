648f7df6a57dfd5e360098eb94658eaa47e8dcc1
Uran <yyul960101@gmail.com>
2025-03-17 00:39:51 +0900

    edit history

--- frontend/src/Introduce/History/HistoryMain.jsx
+++ frontend/src/Introduce/History/HistoryMain.jsx
@@ -1,18 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "../History/HistoryMain.css";
-import historyImage1 from "@/assets/historyimage1.png";
-import historyImage2 from "@/assets/historyimage2.png";
-import img1 from "@/assets/mainImage.png";
+import historyImage2 from "@/assets/Areal/Int/history.png"
 import img2 from "@/assets/PlusMainImage.png";
 
 const historyData = [
   {
-    period: "2011 ~ 2020",
-    image: historyImage1,
-    mainImage: img1, // 추가된 기존 이미지
-  },
-  {
-    period: "2021 ~ 현재",
+    period: "새한의 역사",
     image: historyImage2,
     mainImage: img2, // 추가된 기존 이미지
   },
