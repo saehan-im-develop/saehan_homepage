f32f2d7d651c9ec48ed2267b568839e825d4a1da
uran961029 <yyul960101@gmail.com>
2025-03-18 03:25:44 +0000

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import "@fortawesome/fontawesome-free/css/all.css"; // ✅ 확장자 `.min.css` 제거
+
 
 
 createRoot(document.getElementById('root')).render(
