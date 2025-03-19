9968066a33445889a933e7949ce40f58cf48114c
welcometohelloworld0 <pyh8461@gmail.com>
2025-03-19 10:46:42 +0900

    edit push - pyh

--- frontend/src/Affiliate/HightechComponent/HighEquip.jsx
+++ frontend/src/Affiliate/HightechComponent/HighEquip.jsx
@@ -3,7 +3,6 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Mousewheel } from "swiper/modules";
 import img1 from "@/assets/Areal/high/nh1.jpg";
 import img3 from "@/assets/Areal/high/nh3.png";
-import img6 from "@/assets/Areal/high/nh6.png";
 import img7 from "@/assets/Areal/high/nh7.png";
 import img8 from "@/assets/Areal/high/nh8.png";
 import img9 from "@/assets/Areal/high/nh9.png";
@@ -11,7 +10,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "../CommonCSS/CommonEquip.css";
 
-const images = [img1, img3, img6, img7, img8, img9];
+const images = [img1, img3, img7, img8, img9];
 
 const HighEquip = () => {
     return (
